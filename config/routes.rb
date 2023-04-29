681268cffdbb4e9c7c8471f494aca2767d4624d2
Jamie Ewing <jamesewing0@gmail.com>
2023-04-29 11:44:11 +0100

    question and answer controller, reply and ask done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Generic syntax:
+  # verb "path", to: "controller#action"
+  get "ask", to: "questions#ask"
+  get "reply", to: "answer#reply"
 end
