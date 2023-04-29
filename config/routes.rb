e5e7bf15a002199c1778a34d70e76a39e9ffb87d
Jamie Ewing <jamesewing0@gmail.com>
2023-04-29 12:02:17 +0100

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+# Rails.application.routes.draw do
+#   get "ask", to: "questions#ask"
+#   get "reply", to: "answer#reply"
+# end
+
 Rails.application.routes.draw do
-  # Generic syntax:
-  # verb "path", to: "controller#action"
-  get "ask", to: "questions#ask"
-  get "reply", to: "answer#reply"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get "ask", to: "questions#ask", as: :ask
+  get "answer", to: "questions#answer", as: :answer
 end
